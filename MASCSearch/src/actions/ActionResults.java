02d98801a36e6eaad77b43e2116731f62750d4aa
matthewsaba66 <matthewsaba66@gmail.com>
2015-03-30 11:29:01 +0200

    per andrea, il mio caro amico maiale

---
+++ MASCSearch/src/actions/ActionResults.java
@@ -0,0 +1,45 @@
+package actions;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ActionResults
+ */
+@WebServlet("/indietro")
+public class ActionResults extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ActionResults() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletContext application = getServletContext();
+		RequestDispatcher rd = application.getRequestDispatcher("/searchPage.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ MASCSearch/src/helper/Parser.java
@@ -0,0 +1,159 @@
+package helper;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.util.CharArraySet;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.RAMDirectory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jwat.warc.WarcReader;
+import org.jwat.warc.WarcReaderFactory;
+import org.jwat.warc.WarcRecord;
+
+public class Parser {
+
+	public static void main(String[] args) throws IOException{
+		getIndex();
+	}
+	
+	public static IndexWriter getIndex() throws IOException{
+
+		/* create a standard analyzer */
+		StandardAnalyzer analyzer = new StandardAnalyzer( CharArraySet.EMPTY_SET);
+
+		/* create the index in the pathToFolder or in RAM (choose one) */
+		File file = new File("pathToFolder");
+		Path path = Paths.get("/home/matteo/Scrivania/");
+		Directory index = FSDirectory.open(path);
+		//Directory index =new RAMDirectory();
+
+		/* set an index config */
+		IndexWriterConfig config = new IndexWriterConfig( analyzer);
+		config.setOpenMode(OpenMode.CREATE);
+		/* create the writer */
+		IndexWriter writer = new IndexWriter(index, config);
+
+		/*get file list of warc's to analyze*/
+		List<String> fileList = listFilesForFolder(new File("/home/matteo/CRAWL/Decompressi/"));
+
+		int records = 0;
+		
+	//	for (String warc : fileList){
+
+			InputStream in = new FileInputStream(new File("/home/matteo/CRAWL/Decompressi/00.warc"/* + warc*/ ));
+
+			
+			//int errors = 0;
+
+			WarcReader reader = WarcReaderFactory.getReader( in );
+			WarcRecord record;
+			int j = 0;
+		//	System.out.println("analizzo documento: " + warc);
+			while ((record = reader.getNextRecord()) != null && j!=300) {
+
+				++records;
+
+				if (record.diagnostics.hasErrors()) {
+					Document d = printRecord(record);
+					writer.addDocument(d);
+					j++;
+					System.out.println(j + " sono entrato in un ciclo ");
+					//printRecordErrors(record);
+					//errors += record.diagnostics.getErrors().size();
+				}
+			}
+			System.out.println("--------------");
+			System.out.println("       Records: " + records);
+			//System.out.println("        Errors: " + errors);
+			reader.close();
+			in.close();
+		
+	//	}
+		
+		writer.close();
+
+		return writer;
+
+	}
+
+
+	public static List<String> listFilesForFolder(final File folder) {
+
+		List<String> fileList = new ArrayList<String>();
+		for (final File fileEntry : folder.listFiles()) {
+			if (fileEntry.isDirectory()) {
+				listFilesForFolder(fileEntry);
+			} else {
+				fileList.add(fileEntry.getName());
+			}
+		}
+		return fileList;
+	}
+
+	private static Document printRecord(WarcRecord record) throws IOException {
+		// TODO Auto-generated method stub
+		Document doc = new Document();
+		doc.add(new StringField("type", record.header.contentTypeStr, Field.Store.YES));
+
+		if(record.header.warcTargetUriUri!=null){
+			doc.add(new TextField("url", record.header.warcTargetUriUri.toString(), Field.Store.YES));
+		}
+		//doc.add(new TextField("title",getTitle(record), Field.Store.YES));
+		String[] prova = getPayload(record);
+		doc.add(new TextField("title",prova[0], Field.Store.YES));
+		doc.add(new TextField("body",prova[1], Field.Store.YES));
+
+
+		return doc;
+	}
+
+	private static String[] getPayload(WarcRecord record) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader payin = new BufferedReader(new InputStreamReader(record.getPayload().getInputStream()));
+		String line = "";
+		String nextLine;
+		while((nextLine = payin.readLine()) != null) {
+			line = line.concat(nextLine + "\n");
+		}
+
+		//System.out.println("LINE: ---"+line);
+
+		org.jsoup.nodes.Document doc = Jsoup.parse(line);
+		//line = doc.toString();
+
+
+
+		String[] text = new String[3];
+		text[0] = "Title: ";
+
+		for(Element div : doc.select("title")){
+			text[0] = text[0].concat(div.text()+"\n");
+		}
+		text[1] = "Body: ";
+		for(Element div : doc.select("body")){
+			text[1] = text[1].concat(div.text()+"\n");
+		}
+
+		return text;
+	}
+
+}
